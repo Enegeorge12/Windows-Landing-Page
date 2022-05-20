c55e6bcb56980d538f3b71c04f098c33e2fba6a1
Ene-George <ene.2456@gmail.com>
2022-05-20 08:16:31 +0300

    WindowsPageLayout

---
+++ indexScript.js
@@ -0,0 +1,30 @@
+ // active windows container
+ function activeWindows() {
+    // let activeWindows = document.getElementById("windowsIcon");
+    let windows = document.querySelector(".container");
+    if (windows.style.display === "none") {
+      windows.style.display = "flex";
+      // console.log("inchis");
+    } else {
+      windows.style.display = "none";
+      // console.log("deschis");
+    }
+  }
+  // time clock
+  function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+
+  function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    } // add zero in front of numbers < 10
+    return i;
+  }
